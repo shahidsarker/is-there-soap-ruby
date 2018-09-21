90663ed5ec50f5fcabf7f351c9585464991cf754
Shahid <shahid.sarker@gmail.com>
2018-09-21 16:20:33 -0400

    space view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email)
+
+Space.create(name:'House',description:'Our home.',location:'Queens')
