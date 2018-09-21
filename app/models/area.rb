0beaff1a54a7dcfb74b4ce7ef456ec4ac6103978
Shahid <shahid.sarker@gmail.com>
2018-09-21 13:32:45 -0400

    install activestorage and setup models

--- app/models/area.rb
+++ app/models/area.rb
@@ -2,4 +2,6 @@ class Area < ApplicationRecord
   belongs_to :space
   has_many :area_items, dependent: :destroy
   has_many :items, through: :area_items
+
+  has_one_attached :image
 end
--- app/models/space.rb
+++ app/models/space.rb
@@ -2,4 +2,6 @@ class Space < ApplicationRecord
   has_many :space_users, dependent: :destroy
   has_many :users, through: :space_users
   has_many :areas
+
+  has_one_attached :image
 end
