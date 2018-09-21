a463b16d4a958b7e4fd77732f73ea22f9b3d918c
Shahid <shahid.sarker@gmail.com>
2018-09-21 14:34:48 -0400

    added name field

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  # devise custom field changes
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  # devise
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:name, :email, :password)}
+    devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:name, :email, :password, :current_password)}
+  end
 end
